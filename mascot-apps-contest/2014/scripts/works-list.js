925e63e859f8077919c8739aead79f66307d7f57
kureikei <pronama+kei@live.jp>
2014-12-11 18:52:21 +0900

    update

--- mascot-apps-contest/2014/scripts/works-list.js
+++ mascot-apps-contest/2014/scripts/works-list.js
@@ -361,16 +361,16 @@
     group: "だいこんグループ"
 }, {
     id: "29",
-    title: "「あんずちゃん」(バッテリーモニターのテーマ)",
+    title: "「あんずちゃん」「クエリちゃん」(バッテリーモニターのテーマ)",
     desc: "ノートPC 用のバッテリーモニターアプリです。バッテリーの残量や残り時間を表示できます。オープンソースで開発しており、Windows 用の実行ファイルを配布しています。",
     creator: {
         name: "satofumi",
         twitter: "satofumi_"
     },
     thumb: "029.png",
-    images: ["029_1.png"],
+    images: ["029_1.png", "029_2.png"],
     urls: ["http://hyakuren-soft.sakura.ne.jp/qlm_batmon/"],
-    tags: ["アプリ", "Windows", "美雲あんず"]
+    tags: ["アプリ", "Windows", "美雲あんず", "クエリちゃん"]
     // group: none
 }, {
     id: "30",
